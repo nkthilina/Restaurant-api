789d0c17284b5e6a25637f08788256e0704259f6
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-06-02 11:28:04 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const RestaurantModel = require("./models/Restaurant");
 
 app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/HyperByte_Careers-Test-DB")
@@ -18,6 +21,7 @@ app.get("/api/", (req, res) => {
   res.send("New test API");
 });
 
+// show all restaurants
 app.get("/restaurants", async(req, res) => {
   try {
     const restaurants = await RestaurantModel.find({});
@@ -27,11 +31,82 @@ app.get("/restaurants", async(req, res) => {
   }
 });
 
+// show the relevant restaurant
+app.get("/restaurants/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const restaurant = await RestaurantModel.findById({ _id: id });
+    res.status(200).json(restaurant);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// create new restaurant
 app.post("/restaurants", async (req, res) => {
   try {
     const restaurants = await RestaurantModel.create(req.body);
     res.status(200).json(restaurants);
-  } catch (err) {
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// edit restaurant
+app.patch("/restaurant/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const restaurant = await RestaurantModel.findById({ _id: id });
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found" });
+    }
+    res.status(200).json(restaurant);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// update a restaurant
+// app.put("/updateRestaurant/:id", async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     const restaurant = await RestaurantModel.findByIdAndUpdate({ _id: id }, req.body, { new: true });
+//     if (!restaurant) {
+//       return res.status(404).json({ message: "Restaurant not found" });
+//     }
+//     const updatedRestaurant = await RestaurantModel.findById({ _id: id });
+//     res.status(200).json(updatedRestaurant);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+app.get('/editRestaurant/:id', (req, res) => {
+  const id = req.params.id
+  RestaurantModel.findById({_id: id})
+    .then((restaurant) => res.json(restaurant))
+    .catch((err) => res.json(err));
+})
+app.put('/updateRestaurant/:id',  (req, res) => {
+  // try {
+    const id = req.params.id;
+    RestaurantModel.findByIdAndUpdate({ _id: id }, req.body)
+    .then((updatedRestaurant) => res.json(updatedRestaurant))
+    .catch((err) => res.json(err));
+  // } catch (error) {
+  //   res.status(500).json({ message: error.message });
+  // }
+})
+
+// delete restaurant
+app.delete("/restaurant/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const restaurant = await RestaurantModel.findByIdAndDelete({ _id: id });
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found with that id ${id}." });
+    }
+    res.status(200).json(restaurant);
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 const restaurantSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  telephone: {
-    type: Number,
-    required: true,
-  }
+  name: String,
+  address: String,
+  telephone: Number
+  // name: {
+  //   type: String,
+  //   required: true,
+  // },
+  // address: {
+  //   type: String,
+  //   required: true,
+  // },
+  // telephone: {
+  //   type: Number,
+  //   required: true,
+  // }
 })
 
 const RestaurantModel = mongoose.model("restaurants", restaurantSchema);
